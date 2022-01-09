b4a1ec02ffa0aaa3fe10d4358fea50a7155d7fb7
reza jeffrey <87489530+RezaJeffrey@users.noreply.github.com>
2022-01-09 17:45:53 +0330

    Add files via upload

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'accounts' #namespace
+urlpatterns = [
+
+    path('', views.user_login, name='login'),
+    path('signup/', views.user_signup, name='signup'),
+    path('logout/', views.user_logout, name='logout')
+]
